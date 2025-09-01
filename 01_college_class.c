3ffc848a44890df49c5566e706a5befe983c0873
kavycodes <kavygupta0605@gmail.com>
2025-09-01 13:48:00 +0530

    first

---
+++ 01_college_class.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// two number sum
+int main(){
+    // int a = 10;
+    // int b = 10;
+    // // int a-b; this could not read by compiler
+    // printf("sum is:%d", a+b);
+    // float a=2.5;
+    // printf("%f", a);
+    int a=printf("Hello world\n");
+    printf("%d", a);
+    return 0;
+}
